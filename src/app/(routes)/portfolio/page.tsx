fe1a56d59f7f76402aca92bb24cf934bac5eeb0a
Damian <dg997678@gmail.com>
2024-06-26 23:24:51 -0500

    space

--- src/app/(routes)/portfolio/page.tsx
+++ src/app/(routes)/portfolio/page.tsx
@@ -15,7 +15,7 @@ const PortfolioPage = () => {
 
             <div className="flex flex-col justify-center h-full">
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">
-                    My lastest<span className="font-bold text-secondary">made projects</span>
+                    My lastest <span className="font-bold text-secondary">made projects</span>
                 </h1>
                 <div className='relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4'>
                     {dataPortfolio.map((data) => (
